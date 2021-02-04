2c24e63e8639804fdd39c7ddbdea529768c04b24
Alejandro Autalan <alejandroautalan@gmail.com>
2021-02-04 02:01:04 -0300

    update script.

--- build-and-serve.sh
+++ build-and-serve.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+cd ../pygubu
+python3 setup.py sdist bdist_wheel
+cd ../pygubu-designer
+cp ../pygubu/dist/* ./dist
+
 python3 setup.py sdist bdist_wheel
 cd dist
 python3 -m http.server 8080
