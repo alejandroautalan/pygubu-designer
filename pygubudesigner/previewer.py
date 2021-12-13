89751ab5d33774de8361a774d59f4ff61eca7266
Alejandro Autalan <alejandroautalan@gmail.com>
2021-12-12 21:24:15 -0300

    Removed the method _pre_realize(self, bobject) This method was configuring each widget with a 'class_' property 'PreviewWidget' for making it preview only, but loosing the real winfo_class name required now for the TtkStylePropertyEditor.

--- pygubudesigner/previewer.py
+++ pygubudesigner/previewer.py
@@ -39,7 +39,7 @@ import pygubudesigner.actions as actions
 
 from .widgetdescr import WidgetMeta
 from .widgets.toplevelframe import ToplevelFramePreview
-from stylehandler import StyleHandler
+from .widgets.ttkstyleentry import TtkStylePropertyEditor
 
 
 try:
@@ -58,32 +58,18 @@ class BuilderForPreview(pygubu.Builder):
                      'ttk.Panedwindow.Pane', 'ttk.Notebook.Tab',
                      'pygubudesigner.ToplevelFramePreview']
 
-    def _pre_realize(self, bobject):
-        super(BuilderForPreview, self)._pre_realize(bobject)
-        wmeta = bobject.wmeta
-        cname = wmeta.classname
-        #print('In _pre_process_data', cname)
-
-        #  Do not resize main window when
-        #  Sizegrip is dragged on preview panel.
-        if cname == 'ttk.Sizegrip':
-            wmeta.properties['class_'] = 'DUMMY_CLASS'
-        if cname not in self.normalwidgets:
-            if 'class_' in bobject.properties:
-                wmeta.properties['class_'] = 'PreviewWidget'
-
     def _post_realize(self, bobject):
-        #print('In postProcess', bobject.wmeta.classname)
+        '''Configure widget for "preview" mode.'''
         cname = bobject.wmeta.classname
         if cname not in self.normalwidgets:
             if cname.startswith('tk.Menuitem'):
                 return
             self.make_previewonly(bobject.widget)
+            # TODO: for custom widgets we may need to add a method
+            #   'configure_for_preview(self, widget) in BuilderObject' ?
 
     def make_previewonly(self, w):
         '''Make widget just display with no functionality.'''
-        for child in w.winfo_children():
-            self._crop_widget(child)
         self._crop_widget(w)
 
     def _crop_widget(self, w):
@@ -503,31 +489,12 @@ class PreviewHelper:
         canvas.bind('<5>', lambda event: canvas.yview('scroll', 1, 'units'))
         self._create_indicators()
 
-        # We'll need to re-apply the styles if the style definition file
-        # gets modified, so keep a reference to this method in case the style changes
-        # and we need to run it again.
-        StyleHandler.update_ttk_styles_func = self.apply_ttk_style
-        
         self.style = ttk.Style()
         self.style.configure('PreviewFrame.TFrame', background='lightgreen')
-        
-        # Apply the ttk styles from the ttk style definition file (if the file is there.)
-        self.apply_ttk_style()
 
         self.selected_widget = None
         canvas.bind_all(actions.PREVIEW_TOPLEVEL_CLOSE_ALL,
                         lambda e: self.close_toplevel_previews())
-
-    def apply_ttk_style(self):
-        try:
-            logger.debug("Applied Ttk style definition")
-
-            style_code = StyleHandler._get_ttk_style_definition()
-            if style_code:
-                exec(style_code)
-        except Exception as e:   
-            msg = ('ttk style definition error: %s')
-            logger.error(msg, e)      
 
     def add_resource_path(self, path):
         self.resource_paths.append(path)
@@ -693,6 +660,11 @@ class PreviewHelper:
         x, y = self.canvas.canvasx(x), self.canvas.canvasy(y)
         return (x, y)
 
+    def _update_style_editor(self, widget):
+        '''Setup TtkStylePropertyEditor with the real winfo_class and class name.'''
+        hints = (widget.winfo_class(), widget.__class__.__name__)
+        TtkStylePropertyEditor.set_filter_hints(hints)
+
     def show_selected(self, identifier, selected_id=None):
         canvas = self.canvas
         if selected_id is None:
@@ -714,6 +686,7 @@ class PreviewHelper:
                 x, y = self._calculate_indicator_coords(tag, widget)
                 ox, oy = canvas.coords(tag)
                 canvas.move(tag, x - ox, y - oy)
+            self._update_style_editor(widget)
         self._sel_id = identifier
         self._sel_widget = selected_id
 
