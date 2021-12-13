d1cfccf77f12cd29897888e72fe125f7ace65955
Alejandro Autalan <alejandroautalan@gmail.com>
2021-12-12 21:31:05 -0300

    Removed the population file required to populate the style combobox property. Created the StyleRegister class to capture the styles defined by the user.

--- pygubudesigner/stylehandler.py
+++ pygubudesigner/stylehandler.py
@@ -13,169 +13,95 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-import json
 import logging
-from os import path, stat
-from pygubu import builder
+from os import path
+from tkinter import ttk
 from pygubudesigner import preferences as pref
 from pygubudesigner.widgets.ttkstyleentry import TtkStylePropertyEditor
+from .i18n import translator as _
 
 
 logger = logging.getLogger(__name__)
-CLASS_MAP = builder.CLASS_MAP
+
+
+# StyleRegister
+# This class y used to capture the styles defined by the user.
+#
+# Some widgets still need the option database to fully configure the
+# style apperance (like the TCombobox) so, you can use the master
+# attribute of the style to call the functions option_add, option_get, etc
+#
+class StyleRegister(ttk.Style):
+    STYLE_DEFINITIONS = set()
+
+    def _add_style(self, style):
+        if style not in self.STYLE_DEFINITIONS:
+            self.STYLE_DEFINITIONS.add(style)
+
+    def registered_styles(self):
+        '''Return list of captured styles'''
+        return list(self.STYLE_DEFINITIONS)
+
+    def configure(self, style, query_opt=None, **kw):
+        self._add_style(style)
+        super(StyleRegister, self).configure(style, query_opt, **kw)
+
 
 class StyleHandler:
-    
-    update_ttk_styles_func = None
-    last_modified_definition_file = None
-    last_modified_population_file = None
+    last_definition_file = None
+    last_modified_time = None
 
-    def __init__(self, properties_editor):
-        
-        self.properties_editor = properties_editor
-        self.style_property_editor = None
+    def __init__(self, mframe):
+        self.mframe = mframe
         self.after_token = None
-        
-    @staticmethod
-    def _get_ttk_style_definition():
-        """
-        Return the ttk style code as a string.
-        """
+        self.style = StyleRegister()
+
+    def start_monitoring(self):
+        self.mframe.after_idle(self.check_definition_file)
+
+    def _apply_ttk_styles(self, style_code):
+        logger.debug(_("Applying ttk style definitions"))
+        try:
+            if style_code:
+                exec(style_code, {'style': self.style})
+                new_styles = self.style.registered_styles()
+                TtkStylePropertyEditor.set_global_style_list(new_styles)
+        except Exception as e:
+            msg = _('ttk style definition error: %s')
+            logger.error(msg, e)
+
+    @classmethod
+    def get_ttk_style_difinitions(cls):
         contents = None
-        
-        # Get the path to the style definition file.
         style_definition_path = pref.get_option('v_style_definition_file')
-        
+
         if path.isfile(style_definition_path):
-        
             with open(style_definition_path) as f:
                 contents = f.read()
-                
-            if not StyleHandler.last_modified_definition_file:
-                file_info = stat(style_definition_path)
-                StyleHandler.last_modified_definition_file = file_info.st_mtime
-                
         return contents
-        
-    @staticmethod
-    def _get_ttk_style_populate():
-        """
-        Return the style population code as a dictionary.
-        """
-        data = None
-        
-        # Get the path to the style population file.
-        style_population_path = pref.get_option('v_style_population_file')        
-        
-        if path.isfile(style_population_path):
-            with open(style_population_path) as f:
-                contents = f.read()
-                
-                data = json.loads(contents)
-                
-            if not StyleHandler.last_modified_population_file:
-                file_info = stat(style_population_path)
-                StyleHandler.last_modified_population_file = file_info.st_mtime                
-            
-        return data
 
-    def find_style_property_editor(self, pname, editor):
-        """
-        Find the style editor drop-down so we can populate it in a different method. 
-        """
-
-        # Look for the widget that contains a combobox.
-        # The widget we're looking for is: TtkStylePropertyEditor
-        for c in editor.winfo_children():
-            if isinstance(c, TtkStylePropertyEditor):
-                # We found the drop-down menu widget
-                
-                # Keep track of it so we can update populate it
-                # with a list of styles.
-                self.style_property_editor = c
-                
-                self.populate_ttk_style_list()
-                
-                
-                if self.after_token:
-                    self.style_property_editor.after_cancel(self.after_token)
-                self.after_token = self.style_property_editor.after(1000, self.check_for_file_changes)
-    
-    def populate_ttk_style_list(self):
-
-        style_names = []
-        
-        # Get the user-defined style definitions as a dictionary
-        style_data = self._get_ttk_style_populate()
-        
-        # Get the current style that's selected in the Style combobox
-        # because if the current style is not in the population list,
-        # then the current style combobox text should be cleared.
-        current_style = self.style_property_editor._combobox.get()
-        
-        style_exists_in_population_list = False
-        
-        # If the current widget that is selected is compatible
-        # with one or more styles, list the compatible styles in the 'style'
-        # drop-down.
-        if style_data and isinstance(style_data, dict):
-            for k, v in style_data.items():
-                if self.properties_editor._current.classname in v:
-                    if current_style and current_style == k:
-                        style_exists_in_population_list = True
-                    style_names.append(k)
-        
-        # Update 'style' drop-down menu.
-        self.style_property_editor.parameters(values=style_names)     
-        
-        if not style_exists_in_population_list:
-            # The user has a style selected that is not 
-            # in the style population list. So clear the current style.
-            self.style_property_editor._combobox.set('')
-
-            self.reload_definition_file()
-        
     def check_definition_file(self):
-    
+        # print('checking definitions')
         # Get the path to the style definition file.
-        style_definition_path = pref.get_option('v_style_definition_file')     
-        
-        if path.isfile(style_definition_path):
-            file_info = stat(style_definition_path)
-            
-            if file_info.st_mtime != StyleHandler.last_modified_definition_file:
-                StyleHandler.last_modified_definition_file = file_info.st_mtime
-                self.reload_definition_file()
-                
-                print("Reloaded definition file")
-                
-    def check_population_file(self):
-    
-        # Get the path to the style population file.
-        style_population_path = pref.get_option('v_style_population_file')     
-        
-        if path.isfile(style_population_path):
-            file_info = stat(style_population_path)
-            
-            if file_info.st_mtime != StyleHandler.last_modified_population_file:
-                StyleHandler.last_modified_population_file = file_info.st_mtime
-                self.reload_population_file()
-                
+        style_definition_path = pref.get_option('v_style_definition_file')
 
-                print('Reloaded population file')
-        
-    def reload_definition_file(self):
-        
-        if StyleHandler.update_ttk_styles_func:
-            StyleHandler.update_ttk_styles_func()
-        
-    def reload_population_file(self):
-        
-        self.populate_ttk_style_list()
-        
-    def check_for_file_changes(self):
-        
-        self.check_definition_file()
-        self.check_population_file()
-        self.after_token = self.style_property_editor.after(1000, self.check_for_file_changes)
-        
+        do_reload = False
+        is_new_file = False
+        if path.isfile(style_definition_path):
+            file_mtime = path.getmtime(style_definition_path)
+            if StyleHandler.last_definition_file != style_definition_path:
+                do_reload = True
+                is_new_file = True
+                StyleHandler.last_definition_file = style_definition_path
+                StyleHandler.last_modified_time = file_mtime
+            if not is_new_file:
+                if file_mtime > StyleHandler.last_modified_time:
+                    do_reload = True
+                    StyleHandler.last_modified_time = file_mtime
+        if do_reload:
+            contents = None
+            with open(style_definition_path) as f:
+                contents = f.read()
+            self._apply_ttk_styles(contents)
+        # schedule new check
+        self.after_token = self.mframe.after(1000, self.check_definition_file)
