fe7a2836f86a639d50297f30140cfff7193467da
Alejandro Autalan <alejandroautalan@gmail.com>
2022-03-13 23:13:51 -0300

    Remove usage of TkApplication in examples refs #79

--- examples/menu.py
+++ examples/menu.py
@@ -1,32 +1,29 @@
 # encoding: utf8
 # menu.py
-try:
-    import tkinter as tk
-    from tkinter import messagebox
-except:
-    import Tkinter as tk
-    import tkMessageBox as messagebox
+import tkinter as tk
+from tkinter import messagebox
 import pygubu
 
-class MyApplication(pygubu.TkApplication):
 
-    def _create_ui(self):
-        #1: Create a builder
+class MenuExampleApp:
+
+    def __init__(self, master=None):
+        # 1: Create a builder
         self.builder = builder = pygubu.Builder()
 
-        #2: Load an ui file
+        # 2: Load an ui file
         builder.add_from_file('menu.ui')
 
-        #3: Create the widget using self.master as parent
-        self.mainwindow = builder.get_object('mainwindow', self.master)
+        # 3: Create the widget using self.master as parent
+        self.mainwindow = builder.get_object('mainwindow', master)
 
         # Set main menu
-        self.mainmenu = menu = builder.get_object('mainmenu', self.master)
-        self.set_menu(menu)
+        self.mainmenu = mainmenu = builder.get_object(
+            'mainmenu', self.mainwindow)
+        self.mainwindow.configure(menu=mainmenu)
 
         # Configure callbacks
         builder.connect_callbacks(self)
-
 
     def on_mfile_item_clicked(self, itemid):
         if itemid == 'mfile_open':
@@ -34,14 +31,15 @@ class MyApplication(pygubu.TkApplication):
 
         if itemid == 'mfile_quit':
             messagebox.showinfo('File', 'You clicked Quit menuitem. Byby')
-            self.quit();
-
+            self.mainwindow.quit()
 
     def on_about_clicked(self):
         messagebox.showinfo('About', 'You clicked About menuitem')
 
+    def run(self):
+        self.mainwindow.mainloop()
+
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    app = MyApplication(root)
+    app = MenuExampleApp()
     app.run()
