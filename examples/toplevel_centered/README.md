5471faa724cacd93332b40ab0d5f2bb9c8cfb630
Alejandro Autalán <alejandroautalan@gmail.com>
2022-07-04 09:26:14 -0300

    Add example: center window on screen

---
+++ examples/toplevel_centered/README.md
@@ -0,0 +1,5 @@
+# Toplevel Centered on screen
+
+Shows howto center a toplevel window on screen.
+
+For configurations with multiple monitors see screeninfo package on pypi.
---
+++ examples/toplevel_centered/centered_demo1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import pathlib
+import pygubu
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "demo.ui"
+
+
+class CenteredDemoApp:
+    def __init__(self, master=None):
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+        self.mainwindow = builder.get_object("topmain", master)
+        builder.connect_callbacks(self)
+        
+        # center window on screen after creation
+        self._first_init = True
+        self.mainwindow.bind('<Map>', self.center_window)
+        
+    def center_window(self, event):
+        if self._first_init:
+            print('centering window...')
+            toplevel = self.mainwindow
+            height = toplevel.winfo_height()
+            width = toplevel.winfo_width()
+            x_coord = int(toplevel.winfo_screenwidth() / 2 - width / 2)
+            y_coord = int(toplevel.winfo_screenheight() / 2 - height / 2)
+            geom = f'{width}x{height}+{x_coord}+{y_coord}'
+            toplevel.geometry(geom)
+            self._first_init = False
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+if __name__ == "__main__":
+    app = CenteredDemoApp()
+    app.run()
