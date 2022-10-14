dd8d83156f4944191a41f086f5737af920113429
Alejandro Autalan <alejandroautalan@gmail.com>
2022-10-13 23:53:45 -0300

    Order by name.

--- src/pygubudesigner/properties/manager.py
+++ src/pygubudesigner/properties/manager.py
@@ -2,9 +2,15 @@ from .predefined import PROPERTY_DEFINITIONS
 
 
 class PropertiesManager:
-    @staticmethod
-    def iternames():
-        yield from PROPERTY_DEFINITIONS.keys()
+    _required = ("class", "id")
+
+    @classmethod
+    def iternames(cls):
+        yield from cls._required
+
+        for name in sorted(PROPERTY_DEFINITIONS.keys()):
+            if name not in cls._required:
+                yield name
 
     @staticmethod
     def get_definition(pname):
