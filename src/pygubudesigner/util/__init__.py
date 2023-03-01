27b453b431d284b3901390ee25bba5cd7ac82fe4
Alejandro Autalan <alejandroautalan@gmail.com>
2023-03-01 02:32:28 -0300

    Quick fix to allow dark themes on designer. refs #195 Added support to sv_ttk, ttkthemes, ttkbootstrap (install one of them in your environment) Needs more work!.

--- src/pygubudesigner/util/__init__.py
+++ src/pygubudesigner/util/__init__.py
@@ -89,15 +89,126 @@ def menu_iter_children(menu):
 
 __style = None
 
+has_sv_ttk = False
+has_ttkthemes = False
+has_ttkbootsrap = False
+
+
+class ThemeModule:
+    def theme_names(self) -> list:
+        raise NotImplementedError
+
+    def theme_use(self, name):
+        raise NotImplementedError
+
+    def can_handle(self, name) -> bool:
+        return False
+
+
+class SunValeyThemes(ThemeModule):
+    sv_themes = {"sv_dark": "dark", "sv_light": "light"}
+
+    def __init__(self):
+        sv_ttk.get_theme()
+
+    def theme_names(self) -> list:
+        return list(self.sv_themes.keys())
+
+    def theme_use(self, name):
+        sv_ttk.set_theme(self.sv_themes[name])
+
+    def can_handle(self, name) -> bool:
+        return name in self.sv_themes
+
+
+class ttkthemesModule(ThemeModule):
+    def __init__(self):
+        self.style = ThemedStyle()
+        self.definitions = list(self.style.theme_names())
+
+    def theme_names(self) -> list:
+        return self.definitions
+
+    def theme_use(self, name):
+        self.style.theme_use(name)
+
+    def can_handle(self, name):
+        return name in self.definitions
+
+
+class ttkbModule(ThemeModule):
+    def __init__(self):
+        self.style = ttkb.Style()
+        self.definitions = list(self.style.theme_names())
+
+    def theme_names(self) -> list:
+        return self.definitions
+
+    def theme_use(self, name):
+        self.style.theme_use(name)
+
+    def can_handle(self, name):
+        return name in self.definitions
+
+
+class MultipleThemeModuleManager(ttk.Style):
+    modules = []
+    theme_list = None
+
+    def theme_names(self):
+        if self.theme_list is None:
+            MultipleThemeModuleManager.theme_list = list(super().theme_names())
+            for m in self.modules:
+                themes = m.theme_names()
+                for t in themes:
+                    if t not in self.theme_list:
+                        self.theme_list.append(t)
+        return self.theme_list
+
+    def theme_use(self, name):
+        theme_set = False
+        for m in self.modules:
+            if m.can_handle(name):
+                m.theme_use(name)
+                theme_set = True
+                break
+        if not theme_set:
+            super().theme_use(name)
+
+
+try:
+    import ttkbootstrap as ttkb
+
+    has_ttkbootsrap = True
+except ImportError:
+    pass
+
+try:
+    import sv_ttk
+
+    has_sv_ttk = True
+except ImportError:
+    pass
+try:
+    from ttkthemes.themed_style import ThemedStyle
+
+    has_ttkthemes = True
+except ImportError:
+    pass
+
 
 def get_ttk_style():
     """Use ttkthemes if module is installed"""
     global __style
     if __style is None:
-        try:
-            from ttkthemes.themed_style import ThemedStyle
+        manager = MultipleThemeModuleManager()
+        if has_ttkthemes:
+            manager.modules.append(ttkthemesModule())
+        if has_sv_ttk:
+            manager.modules.append(SunValeyThemes())
+        if has_ttkbootsrap:
+            manager.modules.append(ttkbModule())
 
-            __style = ThemedStyle()
-        except BaseException:
-            __style = ttk.Style()
+        __style = manager
+
     return __style
