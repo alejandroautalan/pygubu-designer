b2972795d9544927fdfc7611da400c80e1692346
Alejandro Autalan <alejandroautalan@gmail.com>
2024-01-08 00:36:27 -0300

    New component palette in a treeview. Needs work.

---
+++ src/pygubudesigner/widgets/treecomponentpalette.py
@@ -0,0 +1,161 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import pygubu
+import pygubu.widgets.simpletooltip as tooltip
+from pygubu.stockimage import StockImage, StockImageException
+from .treecomponentpalettebase import TreeComponentPaletteBase
+
+
+class TreeVisualState:
+    def __init__(self, tree: ttk.Treeview):
+        self.tree: ttk.Treeview = tree
+        self.expanded_nodes = []
+        self.focus = None
+
+    def save(self):
+        self.expanded_nodes = []
+        for item in self.tree.get_children():
+            if self.tree.item(item, "open"):
+                self.expanded_nodes.append(item)
+        self.focus = self.tree.focus()
+
+    def restore(self):
+        for item in self.expanded_nodes:
+            if self.tree.exists(item):
+                self.tree.item(item, open=True)
+        if self.focus and self.tree.exists(self.focus):
+            self.tree.see(self.focus)
+
+
+class TreeComponentPalette(TreeComponentPaletteBase):
+    KEY_PRESS_CB_MILISECONDS = 500
+
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+
+        self._keypress_cbid = None
+        self.cptree.filter_func = self.custom_filter
+        tooltip.create(self.fb_show_alltk, "Show all Tk widgets")
+        self.setup_styles()
+        self.visual_state = TreeVisualState(self.cptree)
+
+    def setup_styles(self):
+        s = ttk.Style(self)
+        s.configure("TreeComponentPalette.Treeview", rowheight=30)
+
+    def custom_filter(
+        self, tree: ttk.Treeview, itemid: str, fvalue: str
+    ) -> bool:
+        txt = tree.item(itemid, "text").lower()
+        match_found = fvalue in txt
+        return match_found
+
+    def on_filter_keypress(self, event=None):
+        if self._keypress_cbid is not None:
+            self.after_cancel(self._keypress_cbid)
+        self._keypress_cbid = self.after(
+            self.KEY_PRESS_CB_MILISECONDS, self._on_filter_keypress_after
+        )
+
+    def _on_filter_keypress_after(self, event=None):
+        self.on_do_filter(event)
+        self._cbid = None
+
+    def on_do_filter(self, event=None):
+        self.cptree.filter_by(self.filter_text_var.get())
+
+    def on_filter_clear(self):
+        self.filter_text_var.set("")
+        self.cptree.filter_by("")
+
+    def on_show_alltk(self):
+        self.build_tree()
+
+    def build_tree(self):
+        self.visual_state.save()
+        self.cptree.filter_remove()
+        items = self.cptree.get_children()
+        self.cptree.delete(*items)
+        self.create_treeview_widget_list()
+        self.cptree.filter_by(self.filter_text_var.get())
+        self.visual_state.restore()
+
+    def create_treelist(self):
+        omit_tagset = {"tk", "ttk"}
+
+        # create unique tag set
+        tagset: set = set()
+        for c in pygubu.builder.CLASS_MAP.keys():
+            wc = pygubu.builder.CLASS_MAP[c]
+            tagset.update((str(tag) for tag in wc.tags))
+        tagset.difference_update(omit_tagset)
+
+        # Put every class in defined sections
+        treelist = []
+        for c in pygubu.builder.CLASS_MAP.keys():
+            wc = pygubu.builder.CLASS_MAP[c]
+
+            is_tk_only = "tk" in wc.tags and "ttk" not in wc.tags
+            if is_tk_only and not self.var_show_alltk.get():
+                # print(f"{wc.classname} is tk only")
+                continue
+            ctags = set((str(tag) for tag in wc.tags)) - omit_tagset
+            sections = tagset & ctags
+            for s in sections:
+                treelist.append((s, wc))
+
+        # sort tags by group and label
+        def by_label(t):
+            return f"{t[0]}{t[1].group}{t[1].label}"
+
+        treelist.sort(key=by_label)
+        return treelist
+
+    def create_treeview_widget_list(self):
+        treelist = self.create_treelist()
+        widgetlisttv = self.cptree
+
+        # Default widget image:
+        default_image = ""
+        try:
+            default_image = StockImage.get("22x22-tk.default")
+        except StockImageException:
+            pass
+
+        # Start building widget tree selector
+        sections = {}
+        for key, wc in treelist:
+            section = key
+            # insert section
+            if section not in sections:
+                sections[section] = widgetlisttv.insert(
+                    "", "end", iid=hash(section), text=section
+                )
+            # insert widget
+            w_image = default_image
+            try:
+                w_image = StockImage.get("22x22-{0}".format(wc.classname))
+            except StockImageException:
+                pass
+
+            label = f" {wc.label}"
+            widgetlisttv.insert(
+                sections[section],
+                "end",
+                text=label,
+                image=w_image,
+                tags="widget",
+                values=(wc.classname,),
+            )
+        widgetlisttv.tag_bind("widget", "<Double-1>", self.on_widgetlist_dclick)
+
+    def on_widgetlist_dclick(self, event):
+        tv = event.widget
+        sel = tv.selection()
+        if sel:
+            item = sel[0]
+            classname = tv.item(item, "values")[0]
+            self.on_add_widget_event(classname)
+
+    def on_add_widget_event(self, cname):
+        print("on_addd_widget", cname)
---
+++ src/pygubudesigner/widgets/treecomponentpalettebase.py
@@ -0,0 +1,84 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from pygubu.widgets.filterabletreeview import FilterableTreeview
+from pygubu.widgets.scrollbarhelper import ScrollbarHelper
+
+
+#
+# Base class definition
+#
+class TreeComponentPaletteBase(ttk.Frame):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        frame1 = ttk.Frame(self)
+        frame1.configure(height=200, width=200)
+        label3 = ttk.Label(frame1)
+        label3.configure(
+            compound="left", font="TkSmallCaptionFont", text="Filter:"
+        )
+        label3.pack(fill="both", side="left")
+        entry2 = ttk.Entry(frame1)
+        self.filter_text_var = tk.StringVar()
+        entry2.configure(textvariable=self.filter_text_var, width=10)
+        entry2.pack(expand=True, fill="x", padx="5 5", side="left")
+        entry2.bind("<KeyPress>", self.on_filter_keypress, add="")
+        button3 = ttk.Button(frame1)
+        self.img_cancelcircle16 = tk.PhotoImage(file="cancel-circle-16.png")
+        button3.configure(
+            image=self.img_cancelcircle16,
+            style="Toolbutton",
+            takefocus=True,
+            width=-2,
+        )
+        button3.pack(side="left")
+        button3.configure(command=self.on_filter_clear)
+        separator1 = ttk.Separator(frame1)
+        separator1.configure(orient="vertical")
+        separator1.pack(fill="y", padx=2, side="left")
+        self.fb_show_alltk = ttk.Checkbutton(frame1, name="fb_show_alltk")
+        self.var_show_alltk = tk.BooleanVar()
+        self.fb_show_alltk.configure(
+            offvalue=False,
+            onvalue=True,
+            style="Toolbutton",
+            text="tk",
+            variable=self.var_show_alltk,
+            width=-2,
+        )
+        self.fb_show_alltk.pack(fill="both", side="left")
+        self.fb_show_alltk.configure(command=self.on_show_alltk)
+        frame1.grid(column=0, row=0, sticky="ew")
+        frame3 = ttk.Frame(self)
+        frame3.configure(height=200, width=200)
+        scrollbarhelper3 = ScrollbarHelper(frame3, scrolltype="both")
+        scrollbarhelper3.configure(usemousewheel=False)
+        self.cptree = FilterableTreeview(
+            scrollbarhelper3.container, name="cptree"
+        )
+        self.cptree.configure(style="TreeComponentPalette.Treeview")
+        self.cptree_cols = []
+        self.cptree_dcols = []
+        self.cptree.configure(
+            columns=self.cptree_cols, displaycolumns=self.cptree_dcols
+        )
+        self.cptree.column(
+            "#0", anchor="w", stretch=True, width=200, minwidth=20
+        )
+        self.cptree.heading("#0", anchor="w", text="Components")
+        self.cptree.pack(expand=True, fill="both", side="top")
+        scrollbarhelper3.add_child(self.cptree)
+        scrollbarhelper3.pack(expand=True, fill="both", side="top")
+        frame3.grid(column=0, row=2, sticky="nsew")
+        self.configure(height=200, padding=2, width=200)
+        self.pack(expand=True, fill="both", side="top")
+        self.rowconfigure(2, weight=1)
+        self.columnconfigure(0, weight=1)
+
+    def on_filter_keypress(self, event=None):
+        pass
+
+    def on_filter_clear(self):
+        pass
+
+    def on_show_alltk(self):
+        pass
