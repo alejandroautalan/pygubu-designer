763d9ff188d5dca81a208441e91e42e132e0c635
Alejandro Autalan <alejandroautalan@gmail.com>
2024-01-12 21:02:22 -0300

    Add tree component palette to the ui file.

---
+++ src/pygubudesigner/services/builders/treecomponentpalettebo.py
@@ -0,0 +1,19 @@
+from pygubu.api.v1 import (
+    BuilderObject,
+    register_widget,
+)
+from pygubudesigner.widgets.treecomponentpalette import TreeComponentPalette
+
+
+class TreeComponentPaletteBO(BuilderObject):
+    class_ = TreeComponentPalette
+
+
+_builder_id = "treecomponentpalettebo.TreeComponentPalette"
+register_widget(
+    _builder_id,
+    TreeComponentPaletteBO,
+    "TreeComponentPalette",
+    ("ttk", "Project Widgets"),
+    public=False,
+)
