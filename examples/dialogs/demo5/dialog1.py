0a6a4b086d63bbd59117b6e9b077dcbd9ee1aadb
Alejandro Autalan <alejandroautalan@gmail.com>
2024-05-22 19:06:15 -0300

    remove code not used.

--- examples/dialogs/demo5/dialog1.py
+++ examples/dialogs/demo5/dialog1.py
@@ -10,13 +10,8 @@ class SimpleDialog1(SimpleDialog1UI):
         super().__init__(master)
 
     def run(self):
+        # call the dialog run function
         self.mainwindow.run()
-
-    def on_modal_fullscreen_clicked(self):
-        value = self.fullscreen_var.get()
-        print("dialog fullscreen:", value)
-        self.mainwindow.toplevel.wm_attributes("-fullscreen", value)
-        self.mainwindow.toplevel.update()
 
     def on_dialog_close(self):
         # The main window does not destroy de dialog,
