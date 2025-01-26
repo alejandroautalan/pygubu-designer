9fb4297f49ec0ec5240e7cb4a4e8e81601d0abca
Alejandro Autalan <alejandroautalan@gmail.com>
2025-01-26 06:43:11 -0300

    Fix filedialog size in high dpi screens (linux)

--- src/pygubudesigner/main.py
+++ src/pygubudesigner/main.py
@@ -210,6 +210,12 @@ class PygubuDesigner:
 
         # Customize OpenFiledialog window
         if sys.platform == "linux":
+            scaling_cmd = f"tk scaling -displayof {self.mainwindow}"
+            tk_scaling = float(self.mainwindow.tk.eval(scaling_cmd))
+            fd_width = int(tk_scaling * 510)
+            fd_height = int(tk_scaling * 262)
+            # print(f"{tk_scaling} {dialog_width} {dialog_height}")
+
             file_dialog_hack = """
 # Show button and hide hidden files
 catch {tk_getOpenFile -badoption}
@@ -220,8 +226,11 @@ set ::tk::dialog::file::showHiddenVar 0
 rename ::tk::dialog::file::Create ::tk::dialog::file::_CreateOriginal
 proc ::tk::dialog::file::Create {w class} {
     eval ::tk::dialog::file::_CreateOriginal $w $class
-    wm geometry $w 680x350
+    wm geometry $w {dialog_geometry}
 }"""
+            file_dialog_hack = file_dialog_hack.replace(
+                "{dialog_geometry}", f"{fd_width}x{fd_height}"
+            )
             self.mainwindow.tk.eval(file_dialog_hack)
 
         #
